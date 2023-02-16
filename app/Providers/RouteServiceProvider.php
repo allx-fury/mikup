5f9e844187a7eb381f57d390aa880af33dc244db
allx-fury <allxfury.af@gmail.com>
2023-02-16 12:00:41 +0700

    setting url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,6 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->domain(\Config::get('app.url', null))
                 ->group(base_path('routes/web.php'));
         });
     }
